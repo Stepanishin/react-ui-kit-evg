4b9999110ed3055f8046164649d11810dcb9dcf0
Евгений Степанишин <stepanishin92@mail.ru>
2023-03-16 19:08:45 +0100

    Fix Modal window

--- src/ModalWindow/ModalWindow.tsx
+++ src/ModalWindow/ModalWindow.tsx
@@ -1,7 +1,52 @@
-import React, { FC, useState } from "react";
+import React, { useState } from "react";
+import "./styles.css";
 
-const HeaderLayout: FC = () => {
-  return <div>ModalWindow</div>;
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+  const [modalState, setModalState] = useState<boolean>(isOpen);
+
+  const closeModal = () => {
+    setModalState(false);
+    onClose();
+  };
+
+  return (
+    <>
+      {modalState && (
+        <div className="modal">
+          <div className="modal-overlay" onClick={closeModal}></div>
+          <div className="modal-body">{children}</div>
+        </div>
+      )}
+    </>
+  );
 };
 
-export default HeaderLayout;
+const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="app">
+      <button onClick={handleOpenModal}>Open Modal</button>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        <h2>Modal Content</h2>
+        <p>This is some content inside the modal window.</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default App;
